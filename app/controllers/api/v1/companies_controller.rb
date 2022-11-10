87d528839ec501514402cf0234a6c2ed377488af
wehmoreira <wehmoreira@gmail.com>
2022-11-10 17:24:23 -0300

    routes and create endpoint

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,22 @@
 module Api
   module V1
     class CompaniesController < ApplicationController
+
+      def create
+        company = Company.new(company_params)
+
+        if company.save
+          render json: company, status: :ok
+        else
+          render json: { errors: company.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def company_params
+        params.permit(:name, :cnpj, :total_employees, :total_systems)
+      end
     end
   end
 end
