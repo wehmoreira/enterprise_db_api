dd97cf034bd17d648cb392c162a83c816b89384f
wehmoreira <wehmoreira@gmail.com>
2024-09-19 11:18:10 -0300

    chore: including scripts for testing

---
+++ scripts/printa.rb
@@ -0,0 +1,11 @@
+#############################
+# Author: 
+# Created At: 
+#
+# Last modifier:
+# Modified At: 
+#
+# Version: 0
+#############################
+
+puts "estou no github!"
---
+++ scripts/printa_e_console_loga.rb
@@ -0,0 +1,11 @@
+#############################
+# Author: 
+# Created At: 
+#
+# Last modifier:
+# Modified At: 
+#
+# Version: 0
+#############################
+print 'estou no github!!!'
+Console.log('estou no github!!!')
